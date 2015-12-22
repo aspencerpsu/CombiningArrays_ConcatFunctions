b967a5fd92bcd8558424559fe42bcd43bbc05849
Akeem Spencer <aspencerpsu@gmail.com>
2015-12-21 19:47:13 -0500

    The files for assignment#1 and assignment#2 are finished and completed with the comments within each script going over what each function/logic/loop does

---
+++ w3d2_basicrubyfunctions.rb
@@ -0,0 +1,29 @@
+def StringRead(string)
+  myHash = {}
+  array = string.downcase.split(' ');
+  print array
+
+  i = 0;
+  while i<array.length
+
+  	if myHash.has_key?(array[i]) == true 		
+  		myHash[array[i]] += 1;
+  	else
+		myHash[array[i]] = 1;
+	end
+	i += 1;	
+  end
+  print myHash
+  # myHash[":{@array}"]
+  print "\n"
+  puts myHash
+
+  the_max = myHash.values.max
+
+  print "The maximum freq. from a word from the string is " <<  myHash.index(the_max) << " with a value of " << the_max.to_s()
+end
+
+
+StringRead("Hungry Hungry Hippos came to play at the swamp. To us the hippos have never been this hungry before. I hope the hippos brought plenty of swampwater. Hooray for hippos!");
+
+
