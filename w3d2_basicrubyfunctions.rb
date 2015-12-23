448055047f8a45d48e26d47fb75a8c255936a623
Akeem Spencer <aspencerpsu@gmail.com>
2015-12-23 06:54:12 -0500

    My game has the scenes set up, but I'm having a hard time realizing if I should run the engine of the game in a separate class?

--- w3d2_basicrubyfunctions.rb
+++ w3d2_basicrubyfunctions.rb
@@ -20,7 +20,7 @@ def StringRead(string)
 
   the_max = myHash.values.max
 
-  print "The maximum freq. from a word from the string is " <<  myHash.index(the_max) << " with a value of " << the_max.to_s()
+  print "\n The maximum freq. from a word from the string is " <<  myHash.index(the_max) << " with a value of " << the_max.to_s()
 end
 
 
@@ -28,4 +28,95 @@ StringRead("Hungry Hungry Hippos came to play at the swamp. \\
   To us the hippos have never been this hungry before. I hope the hippos \\
   brought plenty of swampwater. Hooray for hippos!");
 
+class AkeemGameAdventure 
+  attr_accessor :ammo, :daggers, :potions
 
+  def initialize(ammmo, daggers, potions) 
+    @ammo = 200
+    @daggers = 100
+    @potions = 25
+  end
+
+  def scene1
+    puts "Akeem, Arnold Schwartzenegger and the guy from that rocky movie
+          arrive to the planet DREADHEAD, the mission is to gather enough
+          resource on this odd planet to fire the engine back up and running
+          You send first class lieutenant Akeem to scope the land and see if there's
+          any potential threats..."
+
+    put "As you march through thick terrain of mud and fog, there's an enemy using
+          a cloak who fired a laser beam weapon! Do you retreat or do you stand your 
+            ground and use your weapons?"
+
+    action = gets.chomp.downcase
+
+    if action == "daggers"
+      puts "How many daggers?"
+      dagger_to_use = gets
+      @daggers - daggers_to_use
+      return "scene2"
+    elsif action == "ammo"
+      puts "How much ammo to waste the fool?"
+      ammo_to_use = gets
+      @ammo - ammo_to_use
+      return "scene2"
+    elsif action == "stand your ground"
+      puts "you're losing conciousness from the
+            amount of trauma and exhaustion
+            you need more potion fluid before 
+            to continue"
+       puts "continue?"
+      choice = gets.chomp.downcase
+     if choice == "yes"
+      @potions -= 1
+      return 'scene1'
+     else choice == "no"
+        return "GAME OVER"
+    else
+      print "please follow along with the this linear game"
+      return "scene1"
+  end
+
+  def scene2 
+    puts "Now that you've been in rapid fire with these mysterious
+          enemies, you radio in help from your cronies to destroy 
+          these goons, but the radio is destroyed from the exchange
+          of gunfire you had with that beast."
+    puts "Out comes two more beasts!, what do you do? (stand your ground, ammo, or daggers?)"
+
+    action = gets.chomp.downcase
+
+    if action == "daggers"
+      puts "How many daggers"
+      daggers_to_use = gets.chomp
+      @daggers-daggers_to_use
+      return "scene3"
+    elsif action == "ammo"
+      puts "How much ammunition should be used to"
+      puts "waste these two suckers?"
+      ammo_to_use = gets.chomp
+      @ammo-ammo_to_use
+      return "scene3"
+    elsif action == "stand your ground"
+      puts "you're losing conciousness from 
+            the amount of trauma you're losing 
+            in the gunfight, move on or fight 
+            the good fight?"
+      choice = gets.chomp.downcase
+
+     if choice == "yes"
+      @potions -= 1
+      return 'scene1'
+     else choice == "no"
+        return "GAME OVER"
+    else
+      print "please follow along with the this linear game"
+      return "scene1"
+
+  def start
+    while 
+
+
+  end
+
+      
