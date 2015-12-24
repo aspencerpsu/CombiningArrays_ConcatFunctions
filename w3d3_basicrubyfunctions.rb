f102f49affc475b42959e59ecf9eb8ef0988a669
Akeem Spencer <aspencerpsu@gmail.com>
2015-12-23 19:45:56 -0500

    The comments added for the rotate and the factorial function, explaining what happens each line

--- w3d3_basicrubyfunctions.rb
+++ w3d3_basicrubyfunctions.rb
@@ -1,19 +1,25 @@
 def rotate(array, n)
+	# If the absolute value of the "n" is positive shift right 
 	if n.abs/n == 1
 		g = 0
+		# do until the number is moved to the right
 		while g < n 
 			x = array.pop()
+			# insert the popped value into the first array item
 			array.insert(0,x)
 			g += 1
 		end
+		puts '\n'
 		print array
 	else
 		g = 0
 		while g < n.abs
+			# insert the popped value of the first array into the last element
 			x = array.shift()
 			array.insert(-1,x)
 			g += 1
 		end
+		puts '\n'
 		print array
 	end
 end
@@ -24,20 +30,24 @@ rotate([1,2,3,4,5,6], -2)
 rotate(['monkeys', 'alligators','tigers','bears','zebras', 'ponies'], 3)
 
 def factorial(number)
-	while true
-		for i in 1..number 
-			deinc = number-1
-			number *= decinc
-			deinc -= 1
-	  	if i == 0
-	  		return false
-	  	else
-	  		break
-	  	end
+	# Init the counter with a number one less than the original to start
+	deinc = number-1
+	for i in 1..number 
+		# The number will always multiply by the next interation of the deinc
+		number *= deinc
+		deinc -= 1
+		# return false if deinc == 0
+		if deinc == 0 
+			puts
+			print number
+		else 
+			# continue with the next step within the factorial 
+			next
+		end
 	end
 end
 
 
-puts factorial(18)
+factorial(4)
 
 
