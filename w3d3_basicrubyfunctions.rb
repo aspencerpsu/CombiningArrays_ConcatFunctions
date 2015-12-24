8299fb6aa5ef45f1cea41f9d3af6c71404f4b2d6
Akeem Spencer <aspencerpsu@gmail.com>
2015-12-24 09:37:52 -0500

    added extra line breaks, created undefined if a value is neg

--- w3d3_basicrubyfunctions.rb
+++ w3d3_basicrubyfunctions.rb
@@ -2,15 +2,15 @@ def rotate(array, n)
 	# If the absolute value of the "n" is positive shift right 
 	if n.abs/n == 1
 		g = 0
-		# do until the number is moved to the right
+		# do until the number is moved to the right n places
 		while g < n 
 			x = array.pop()
 			# insert the popped value into the first array item
 			array.insert(0,x)
 			g += 1
 		end
-		puts '\n'
-		print array
+		puts 
+		print "\n #{array}"
 	else
 		g = 0
 		while g < n.abs
@@ -19,30 +19,32 @@ def rotate(array, n)
 			array.insert(-1,x)
 			g += 1
 		end
-		puts '\n'
-		print array
+		print "\n #{array}"
 	end
 end
 
 rotate(['cat', 'mouse', 'rabbit', 'dear'], 2)
-print
 rotate([1,2,3,4,5,6], -2)
 rotate(['monkeys', 'alligators','tigers','bears','zebras', 'ponies'], 3)
 
 def factorial(number)
-	# Init the counter with a number one less than the original to start
-	deinc = number-1
-	for i in 1..number 
-		# The number will always multiply by the next interation of the deinc
-		number *= deinc
-		deinc -= 1
-		# return false if deinc == 0
-		if deinc == 0 
-			puts
-			print number
-		else 
-			# continue with the next step within the factorial 
-			next
+	if number < 0
+		print "\n undefined"
+	else
+		# Init the counter with a number one less than the original to start
+		deinc = number-1
+		for i in 1..number 
+			# The number will always multiply by the next interation of the deinc
+			number *= deinc
+			deinc -= 1
+			# return false if deinc == 0
+			if deinc == 0 
+				puts
+				print "\n #{number}"
+			else 
+				# continue with the next step within the factorial 
+				next
+			end
 		end
 	end
 end
